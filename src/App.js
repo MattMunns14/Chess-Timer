8d9961485d269c92be85cddadd7fd5b646a229b6
MattMunns14 <40066916+MattMunns14@users.noreply.github.com>
2019-06-24 12:29:01 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,83 +1,146 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import Container from '@material-ui/core/Container';
-import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid'; 
+import Container from '@material-ui/core/Container'   
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
 import Box from '@material-ui/core/Box';
+
 
 
 import './App.css';
 
-
-
-class App extends React.Component{
+class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      quote: []
-    }
-    this.getNewQuote = this.getNewQuote.bind(this);
+      playerOneMinutes: 0,
+      playerOneSeconds: 0,
+      playerTwoMinutes: 0,
+      playerTwoSeconds: 0,
+      turn: 'One'
+    };
+    this.updateGameTimeMinutes = this.updateGameTimeMinutes.bind(this);
+    this.updateGameTimeSeconds = this.updateGameTimeSeconds.bind(this);
+    this.startTurn = this.startTurn.bind(this);
   }
-  getNewQuote(){
-    fetch('https://quota.glitch.me/random', 
-  )
-    .then(res => res.json())
-    .then((data) => {
-      this.setState({ quote: data })
-    })
-    .catch(console.log)
-
-
+  startTurn(player){
+    console.log(player);
   }
-  componentDidMount() {
-    fetch('https://quota.glitch.me/random')
-    .then(res => res.json())
-    .then((data) => {
-      this.setState({ quote: data })
-      
+  updateGameTimeMinutes(e){
+    this.setState({
+      playerOneMinutes: e.target.value,
+      playerOneSeconds: this.state.playerOneSeconds,
+      playerTwoMinutes: e.target.value,
+      playerTwoSeconds: this.state.playerTwoSeconds,
+      turn: this.state.turn,
+
     })
-    .catch(console.log)
+  };
+  updateGameTimeSeconds(e){
+    this.setState({
+      playerOneMinutes: this.state.playerOneMinutes,
+      playerOneSeconds: e.target.value,
+      playerTwoMinutes: this.state.playerTwoMinutes,
+      playerTwoSeconds: e.target.value,
+      turn: this.state.turn,
+
+    })
   }
   render(){
     return (
-      <Container maxWidth="sm">
-        <Box m={2} p={3}>
-      <Card>
-
       <div className="App">
-        <div className = "quoteBox">
-          <QuoteBox quote={this.state.quote.quoteText} author={this.state.quote.quoteAuthor}/>
-          <NewQuote handleClick = {this.getNewQuote}/>
-        </div>
-
-      </div>
-      </Card>
-      </Box>
+      <Container maxWidth='sm' >
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Paper>
+              <SetTime title={'Time Per Turn'} 
+                       minutes={this.state.playerOneMinutes} 
+                       seconds={this.state.playerOneSeconds}
+                       onChangeMinutes={this.updateGameTimeMinutes}
+                       onChangeSeconds={this.updateGameTimeSeconds}
+                       />
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper><Clock name={'Player 1'} 
+                          minutes={this.state.playerOneMinutes}
+                          seconds={this.state.playerOneSeconds}
+                          startTurn={this.startTurn}/>
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper><Clock name={'Player 2'} 
+                          minutes={this.state.playerTwoMinutes}
+                          seconds={this.state.playerTwoSeconds}
+                          startTurn={this.startTurn}/>
+            </Paper>
+          </Grid>
+        </Grid>
       </Container>
+      </div>
     );
+  }
+
+}
+
+
+class Clock extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      secondsRemaining: this.props.seconds,
+      minutesRemaining: this.props.minutes,
+    }
+  }
+  render(){
+    return(
+      <div className='Clock'>
+      <Typography variant="h6">
+        {this.props.name}
+      </Typography>
+      <Box p={1}>
+        <div>
+          Time Remaining: {this.state.minutesRemaining}:{this.state.secondsRemaining}
+        </div>
+      </Box>
+      <Box p={1}>
+        <Button variant="contained" color="primary" className='Start' onClick={() => this.props.startTurn(this.props.name)}>Begin Turn</Button>
+      </Box>
+    </div>
+    )
   }
 }
 
+
+function SetTime(props){
+ return(
+  <div className='SetTime'>
+    <Typography variant="h4" component="h2">
+      {props.title}
+    </Typography>
+
+    <Typography variant="h5" component="h2">
+
+      <Box p={2}>
+        <Input className='SetTimeInputMinutes' 
+               type='number' 
+               placeholder={props.minutes.toString()}
+               onChange = {props.onChangeMinutes}/> Minutes
+      </Box>
+
+      <Box p={2}>
+        <Input className='SetTimeInputSeconds' 
+               type='number' 
+               placeholder={props.seconds.toString()}
+               onChange={props.onChangeSeconds}/> Seconds
+      </Box>
+
+    </Typography>
+
+  </div>
+ );
+}
+
 export default App;
-
-function QuoteBox(props){
-return (
-  <div className="quoteBox">
-          <Typography variant="h5" component="h2">
-          {props.quote}
-          </Typography>
-          <Typography variant="h6" component="h2">
-          - {props.author}
-          </Typography>
-  </div>);
-}
-
-function NewQuote(props){
-  return (
-    <div className = "newQuote">
-      <Button variant="contained" color="primary" onClick = {props.handleClick}>
-        New Quote</Button>
-    </div>
-  )
-}
