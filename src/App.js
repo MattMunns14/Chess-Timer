fd2b7d81de40238236b4d6ed5233f0b15733be30
MattMunns14 <40066916+MattMunns14@users.noreply.github.com>
2019-06-24 12:28:28 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,118 +1,83 @@
 import React from 'react';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid'; 
-import Container from '@material-ui/core/Container'   
-import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
-import Input from '@material-ui/core/Input';
+import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-
 
 
 import './App.css';
 
-class App extends React.Component {
+
+
+class App extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      playerOneMinutes: 0,
-      playerOneSeconds: 0,
-      playerTwoMinutes: 0,
-      playerTwoSeconds: 0,
-    };
-    this.updateGameTimeMinutes = this.updateGameTimeMinutes.bind(this);
-    this.updateGameTimeSeconds = this.updateGameTimeSeconds.bind(this);
+      quote: []
+    }
+    this.getNewQuote = this.getNewQuote.bind(this);
   }
-  updateGameTimeMinutes(e){
-    this.setState({
-      playerOneMinutes: e.target.value,
-      playerOneSeconds: this.state.playerOneSeconds,
-      playerTwoMinutes: e.target.value,
-      playerTwoSeconds: this.state.playerTwoSeconds,
-
+  getNewQuote(){
+    fetch('https://quota.glitch.me/random', 
+  )
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ quote: data })
     })
-  };
-  updateGameTimeSeconds(e){
-    this.setState({
-      playerOneMinutes: this.state.playerOneMinutes,
-      playerOneSeconds: e.target.value,
-      playerTwoMinutes: this.state.playerTwoMinutes,
-      playerTwoSeconds: e.target.value,
+    .catch(console.log)
 
+
+  }
+  componentDidMount() {
+    fetch('https://quota.glitch.me/random')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ quote: data })
+      
     })
+    .catch(console.log)
   }
   render(){
     return (
+      <Container maxWidth="sm">
+        <Box m={2} p={3}>
+      <Card>
+
       <div className="App">
-      <Container maxWidt='sm' >
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <Paper>
-              <SetTime title={'Time Per Turn'} 
-                       minutes={this.state.playerOneMinutes} 
-                       seconds={this.state.playerOneSeconds}
-                       onChangeMinutes={this.updateGameTimeMinutes}
-                       onChangeSeconds={this.updateGameTimeSeconds}
-                       />
-            </Paper>
-          </Grid>
-          <Grid item xs={6}>
-            <Paper><Clock name={'Player 1'}/></Paper>
-          </Grid>
-          <Grid item xs={6}>
-            <Paper><Clock name={'Player 2'}/></Paper>
-          </Grid>
-        </Grid>
-      </Container>
+        <div className = "quoteBox">
+          <QuoteBox quote={this.state.quote.quoteText} author={this.state.quote.quoteAuthor}/>
+          <NewQuote handleClick = {this.getNewQuote}/>
+        </div>
+
       </div>
+      </Card>
+      </Box>
+      </Container>
     );
   }
-
-}
-
-function Clock(props){
-  return(
-    <div className='Clock'>
-      <Button variant="contained" color="primary" className='Start'>Start</Button>
-      <Timer />
-    </div>
-  );
-}
-
-
-function Timer(props){
-  return(
-    <div/>
-  );
-}
-
-function SetTime(props){
- return(
-  <div className='SetTime'>
-    <Typography variant="h4" component="h2">
-      {props.title}
-    </Typography>
-
-    <Typography variant="h5" component="h2">
-
-      <Box p={2}>
-        <Input className='SetTimeInputMinutes' 
-               type='number' 
-               placeholder={props.minutes.toString()}
-               onChange = {props.updateGameTimeMinutes}/> Minutes
-      </Box>
-
-      <Box p={2}>
-        <Input className='SetTimeInputSeconds' 
-               type='number' 
-               placeholder={props.seconds.toString()}
-               onChange={props.updateGameTimeSeconds}/> Seconds
-      </Box>
-
-    </Typography>
-
-  </div>
- );
 }
 
 export default App;
+
+function QuoteBox(props){
+return (
+  <div className="quoteBox">
+          <Typography variant="h5" component="h2">
+          {props.quote}
+          </Typography>
+          <Typography variant="h6" component="h2">
+          - {props.author}
+          </Typography>
+  </div>);
+}
+
+function NewQuote(props){
+  return (
+    <div className = "newQuote">
+      <Button variant="contained" color="primary" onClick = {props.handleClick}>
+        New Quote</Button>
+    </div>
+  )
+}
